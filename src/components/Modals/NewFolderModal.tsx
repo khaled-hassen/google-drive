72e47f1f872782eb399d3499e3365db50cb08b43
khaled <khaledhassen@proton.me>
2024-04-28 17:09:58 +0200

    make folder name required

--- src/components/Modals/NewFolderModal.tsx
+++ src/components/Modals/NewFolderModal.tsx
@@ -25,9 +25,10 @@ const NewFolderModal: React.FC<Props> = ({
   async function createNewFolder(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     try {
-      setLoading(true);
       const formData = new FormData(event.currentTarget);
       const name = formData.get("name") as string;
+      if (!name) return toast.error("Folder name is required");
+      setLoading(true);
       const folderId = await createFolder(name, id);
       setLoading(false);
       if (!folderId) return;
@@ -42,7 +43,7 @@ const NewFolderModal: React.FC<Props> = ({
   return (
     <Modal title="Create new folder" className="w-96" isOpen={isOpen}>
       <form className="flex flex-col gap-10" onSubmit={createNewFolder}>
-        <label className="dark:bg-lightDark w-full max-w-xl rounded-full bg-darkerWhite px-4 py-2 transition-colors">
+        <label className="w-full max-w-xl rounded-full bg-darkerWhite px-4 py-2 transition-colors dark:bg-lightDark">
           <input
             autoFocus
             type="text"
